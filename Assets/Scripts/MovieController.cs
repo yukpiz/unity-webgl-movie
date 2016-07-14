fe86dd1db57f405365db900ecd83a0c40ca5675f
Yuuki Zayasu <zayasu@act9.co.jp>
2016-07-14 19:17:28 +0900

    Added script.

---
+++ Assets/Scripts/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieController : MonoBehaviour {
+
+    public GameObject cube;
+    private WebGLMovieTexture webGLMovieTexture;
+
+	// Use this for initialization
+	void Start () {
+        webGLMovieTexture = new WebGLMovieTexture("StreamingAssets/Chrome_ImF.mp4");
+        cube.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Diffuse"));
+        cube.GetComponent<MeshRenderer>().material.mainTexture = webGLMovieTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (webGLMovieTexture.isReady)
+        {
+            webGLMovieTexture.Update();
+        }
+	}
+}
